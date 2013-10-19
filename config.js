53365c54936a7f825d4b79f80a4547db8eb97900
Ram Ganesan Athreya <ramgnsn5@gmail.com>
2013-10-19 18:46:25 +0530

    Modified to be suitable for heroku based on tutorial

--- config.js
+++ config.js
@@ -2,13 +2,13 @@
 var config = {
     dev: {
         mode: 'development',
-        port: 80
+        port: 8080
     },
     production: {
         mode: 'production',
-        port: 80
+        port: 8080
     }
 };
 module.exports = function(mode) {
     return config[mode || process.argv[2] || 'dev'] || config.dev;
-}
+};
